7d0c6f8dd94967426104aa9aec3cd36e46a7b2d7
DanielleM <dlmedeiro@gmail.com>
2023-07-18 21:28:08 -0700

    chore: update notes to match database

--- src/forms/CreateEventForm.tsx
+++ src/forms/CreateEventForm.tsx
@@ -1,6 +1,6 @@
 // Purpose: Form for organizers to create an event
 
 // Functionality:
-// - Organizer Inputs (update as needed): Name of Event, In person vs virtual (dropdown sending true for virtual, false for none), Start Date, End Date, Time, Tags, Price, Description (optional), Image (optional)
+// - Organizer Inputs (update as needed): Name of Event, In person vs virtual (dropdown sending true for virtual, false for in person), location, Start Date/time, End Date/time, Event Tags, Price, Description (optional), Image (optional)
 // - EventCreated Modal on submission
 // - Redirect to Profile on completion
