66d13749dd0e014603239ed4f3bab9c05fc9b933
DanielleM <dlmedeiro@gmail.com>
2023-07-20 07:37:57 -0700

    fix: start file structure updates based on next.js docs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import MobileLanding from "../mobile/pages/MobileLanding";
+import MobileLanding from '../mobile/pages/MobileLanding'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <MobileLanding />
     </main>
-  );
+  )
 }
