d0a3c76fa48f86333a1d8e9aa93e545b674cb70b
DanielleM <dlmedeiro@gmail.com>
2023-07-20 08:00:08 -0700

    chore: add temporary links to pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
 import MobileLanding from '../mobile/pages/MobileLanding'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <MobileLanding />
+      <h2 className="title">
+        <Link href="/auth/login">Login</Link>
+      </h2>
+      <h2 className="title">
+        <Link href="/auth/create-customer">Create Customer</Link>
+      </h2>
+      <h2 className="title">
+        <Link href="/auth/create-organizer">Create Organizer</Link>
+      </h2>
+      <h2 className="title">
+        <Link href="/user/profile">Profile</Link>
+      </h2>
     </main>
   )
 }
