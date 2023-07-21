1db89c410a0c745b32d4b478b0805dfb9db74c09
DanielleM <dlmedeiro@gmail.com>
2023-07-21 01:08:38 -0700

    feat: initial create organizer form

--- src/app/user/create-organizer/page.tsx
+++ src/app/user/create-organizer/page.tsx
@@ -4,19 +4,70 @@
 // - Organizer Inputs (update as needed): Name, Email, Password Image (optional)
 // - Welcome Modal on submission
 // - Redirect to Profile on completion
+'use client'
 import React from 'react'
 import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 
-function CreateOrganizerAccountForm() {
+export default function CreateOrganizerAccountForm() {
+  const router = useRouter()
+  // Handles the submit event on form submit.
+  const handleSubmit = async (event: any) => {
+    // Stop the form from submitting and refreshing the page.
+    event.preventDefault()
+
+    // Get data from the form.
+    const data = {
+      organizerName: event.target.organizerName.value,
+      email: event.target.email.value,
+      password: event.target.password.value,
+    }
+
+    // // Send the data to the server in JSON format.
+    // const JSONdata = JSON.stringify(data)
+
+    alert(`Account created for ${data.organizerName}`)
+
+    router.push('/user/profile')
+
+    // // API endpoint where we send form data.
+    // const endpoint = '/api/createOrganizer'
+
+    // // Form the request for sending data to the server.
+    // const options = {
+    //   // The method is POST because we are sending data.
+    //   method: 'POST',
+    //   // Tell the server we're sending JSON.
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //   },
+    //   // Body of the request is the JSON data we created above.
+    //   body: JSONdata,
+    // }
+
+    // // Send the form data to our forms API on Vercel and get a response.
+    // const response = await fetch(endpoint, options)
+
+    // // Get the response data from server as JSON.
+    // // If server returns the name submitted, that means the form works.
+    // const result = await response.json()
+    // alert(`Is this your full name: ${result.data}`)
+  }
   return (
     <>
-      <h1>CreateOrganizerAccountForm</h1>
-
+      <h1>Create Organizer Page</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="organizerName">Organizer Name</label>
+        <input type="text" id="organizerName" name="organizerName" required />
+        <label htmlFor="email">Email</label>
+        <input type="email" id="email" name="email" required />
+        <label htmlFor="password">Password</label>
+        <input type="password" id="password" name="email" required />
+        <button type="submit">Create Account</button>
+      </form>
       <h2>
         <Link href="/">← Back to home</Link>
       </h2>
     </>
   )
 }
-
-export default CreateOrganizerAccountForm
--- src/app/user/login/page.tsx
+++ src/app/user/login/page.tsx
@@ -11,7 +11,13 @@ function LoginForm() {
   return (
     <>
       <h1>Login Page</h1>
-
+      <form action="/send-data-here" method="post">
+        <label htmlFor="first">First name:</label>
+        <input type="text" id="first" name="first" />
+        <label htmlFor="last">Last name:</label>
+        <input type="text" id="last" name="last" />
+        <button type="submit">Submit</button>
+      </form>
       <h2>
         <Link href="/">← Back to home</Link>
       </h2>
