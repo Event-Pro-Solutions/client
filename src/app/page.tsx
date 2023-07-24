89f9aa57cc1678e0b450fe6bc8e5e35586dd7aa2
DanielleM <dlmedeiro@gmail.com>
2023-07-24 11:32:23 -0700

    fix: change file name to resolve file naming error/conflict

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import MobileLanding from '../components/mobile/mobileLanding'
+import MobileLanding from '../components/mobile/mobileLandingPage'
 import Link from 'next/link'
 
 export default function Home() {
---
+++ src/components/mobile/mobileLandingPage.tsx
@@ -0,0 +1,18 @@
+import React, { memo } from 'react'
+
+import { CarouselComponent } from '../carouselComponent'
+
+const MobileLanding = memo(() => {
+  return (
+    <div>
+      <div>Navbar Placeholder</div>
+      <div>Search Bar Placeholder</div>
+      <CarouselComponent />
+    </div>
+  )
+})
+
+MobileLanding.displayName = 'MobileLanding'
+
+// export const MemoizedMobileLanding = React.memo(MobileLanding);
+export default MobileLanding
