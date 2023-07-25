1046456237c48493f9c8f3cd08ac318ea4970b49
Scott E Thompson <sthompson082290@gmail.com>
2023-07-25 15:45:15 -0500

    Created custom hook useViewport to render each page based on viewport width

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
+"use client";
+import DesktopLanding from "@/components/desktop/DesktopLanding";
 import MainLanding from "@/components/directory/MainLanding";
-import { ViewportProvider } from "@/hook/useViewport";
+import MobileLanding from "@/components/mobile/MobileLanding";
+import { ViewportProvider, useViewport } from "@/hook/useViewport";
 
 export default function Home() {
+  const { width } = useViewport();
+  const breakpoint = 600;
+
   return (
-    <ViewportProvider>
-      <main className="flex min-h-screen flex-col items-center justify-between p-0 selection:bg-accent selection:text-neutral">
-        <MainLanding />
-      </main>
-    </ViewportProvider>
+    <main className="flex min-h-screen flex-col items-center justify-between p-0 selection:bg-accent selection:text-neutral">
+      {width < breakpoint ? <MobileLanding /> : <DesktopLanding />}
+    </main>
   );
 }
