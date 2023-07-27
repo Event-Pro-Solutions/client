0910ef73c15ca6e231c08cf944179c6cd2a5be14
DanielleM <dlmedeiro@gmail.com>
2023-07-26 18:47:53 -0700

    fix: add viewProvider to layout and useView to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,16 @@
 import DesktopLanding from "@/components/desktop/DesktopLanding";
 import MainLanding from "@/components/directory/MainLanding";
 import MobileLanding from "@/components/mobile/MobileLanding";
-import { ViewportProvider } from "@/hook/useViewport";
+import { useViewport } from "@/hook/useViewport";
 
 export default function Home() {
+  const { width, height } = useViewport();
   return (
-    <ViewportProvider>
+  
       <main className="flex min-h-screen flex-col items-center justify-between p-0 selection:bg-accent selection:text-neutral">
         {/* {width < breakpoint ? <MobileLanding /> : <DesktopLanding />} */}
         <MainLanding />
       </main>
-    </ViewportProvider>
+   
   );
 }
