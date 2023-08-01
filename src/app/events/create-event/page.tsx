9904a3badf365ef5ddda5054f87f48677d78c6ca
DanielleM <dlmedeiro@gmail.com>
2023-08-01 16:58:44 -0700

    feat: add date and time inputs

--- src/app/events/create-event/page.tsx
+++ src/app/events/create-event/page.tsx
@@ -6,15 +6,89 @@
 // - Redirect to Profile on completion
 'use client'
 
-import React from 'react'
+import React , {useState, useEffect, useRef} from 'react'
 import Link from 'next/link'
 import { useRouter } from 'next/navigation'
 import Image from 'next/image'
 import ticket from '../../../utils/images/ticket.jpg'
+import DateTimePicker from '@/components/DateTimePicker'
+
 
 function CreateEventForm() {
+
   const router = useRouter()
   // Handles the submit event on form submit.
+  const [virtual, setVirtual] = useState(true)
+  const [price, setPrice] = useState('');
+
+  const formatNumber = (n: string) => {
+    // format number 1000000 to 1,234,567
+    return n.replace(/\D/g, '').replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  };
+
+  const formatCurrency = (inputValue: string, blur?: string) => {
+    // appends $ to value, validates decimal side
+    // and puts cursor back in the right position.
+
+    // don't validate empty input
+    if (inputValue === '') {
+      return inputValue;
+    }
+
+    // check for decimal
+    if (inputValue.indexOf('.') >= 0) {
+      // get position of the first decimal
+      // this prevents multiple decimals from
+      // being entered
+      const decimal_pos = inputValue.indexOf('.');
+
+      // split number by the decimal point
+      let left_side = inputValue.substring(0, decimal_pos);
+      let right_side = inputValue.substring(decimal_pos);
+
+      // add commas to the left side of the number
+      left_side = formatNumber(left_side);
+
+      // validate the right side
+      right_side = formatNumber(right_side);
+
+      // On blur, make sure 2 numbers after the decimal
+      if (blur === 'blur') {
+        right_side += '00';
+      }
+
+      // Limit the decimal to only 2 digits
+      right_side = right_side.substring(0, 2);
+
+      // join number by "."
+      return '$' + left_side + '.' + right_side;
+    } else {
+      // no decimal entered
+      // add commas to the number
+      // remove all non-digits
+      const formattedValue = formatNumber(inputValue);
+      return '$' + formattedValue + (blur === 'blur' ? '.00' : '');
+    }
+  };
+
+  const handlePriceChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const inputValue = event.target.value;
+    setPrice(formatCurrency(inputValue));
+  };
+
+  const handleBlur = () => {
+    setPrice((prevValue) => formatCurrency(prevValue, 'blur'));
+  };
+
+function displayText(choice: string) {
+  if (choice == "yes") {
+      setVirtual(true)
+  } else if (choice == "no"){
+    setVirtual(false)
+  }
+}
+console.log(virtual)
+
   const handleSubmit = async (event: any) => {
     // Stop the form from submitting and refreshing the page.
     event.preventDefault()
@@ -61,7 +135,7 @@ function CreateEventForm() {
 
   return (
     <div className="min-h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8 ">
-    <div className="bg-white max-w-md w-full space-y-8 text-dark border-solid border-2 border-primary">
+    <div className="bg-white max-w-lg w-full space-y-8 text-dark border-solid border-2 border-primary">
       <div>
         <Image
           className="mx-auto h-12 w-auto mt-4"
@@ -73,19 +147,21 @@ function CreateEventForm() {
           Create a new Event
         </h2>
       </div>
-      <div className="max-w-md w-full space-y-8">
+      <div className="max-w-lg w-full space-y-8">
         <form
-          className="shadow-md rounded px-8 pt-6 pb-8"
+          className="rounded px-8 pt-6 pb-8"
           onSubmit={handleSubmit}
         >
-          <div className="mb-4">
+          <div className="mb-4 relative">
             <label
-              className="block text-dark text-sm font-bold mb-2"
+
+              className="block text-dark text-sm font-bold mb-2  text-center"
               htmlFor="eventName"
             >
               Event Name
             </label>
             <input
+
               className="shadow appearance-none border rounded border-accent w-full py-2 px-3 text-dark leading-tight focus:outline-secondary focus:shadow-outline placeholder-accent"
               placeholder="Event Name"
               type="text"
@@ -94,73 +170,59 @@ function CreateEventForm() {
               required
             />
           </div>
-
           <div className="mb-4">
             <label
-              className="block text-dark text-sm font-bold mb-2"
-              htmlFor="virtual"
+              className="block text-dark text-sm font-bold mb-2  text-center"
+              htmlFor="description"
             >
-              Virtual or In Person
+              Event Details
             </label>
             <input
               className="shadow appearance-none border rounded border-accent w-full py-2 px-3 text-dark leading-tight focus:outline-secondary focus:shadow-outline placeholder-accent"
-              placeholder="Virtual or In Person"
+              placeholder="Event Description"
               type="text"
-              name="virtual"
+              id="description"
+              name="description"
               required
-              id="virtual"
             />
           </div>
 
-          <div className="mb-4">
+          {/* Date and Time picker */}
+          <div>
             <label
-              className="block text-dark text-sm font-bold mb-2"
-              htmlFor="location"
-            >
-              Location
-            </label>
-            <input
-              className="shadow appearance-none border rounded border-accent w-full py-2 px-3 text-dark leading-tight focus:outline-secondary focus:shadow-outline placeholder-accent"
-              placeholder="Location"
-              name="location"
-              required
-              type="text"
-              id="location"
-            />
-          </div>
-
-          <div className="mb-4">
-            <label
-              className="block text-dark text-sm font-bold mb-2"
+              className="block text-dark text-sm font-bold mb-2 text-center"
               htmlFor="startDateTime"
             >
-              Start Date and Time
-            </label>
-            <input
-              className="shadow appearance-none border rounded border-accent w-full py-2 px-3 text-dark leading-tight focus:outline-secondary focus:shadow-outline placeholder-accent"
-              placeholder="Start Date and Time"
-              type="text"
-              id="startDateTime"
-              name="startDateTime"
-              required
-            />
-          </div>
-          <div className="mb-4">
-            <label
-              className="block text-dark text-sm font-bold mb-2"
-              htmlFor="endDateTime"
-            >
-              End Date and Time
-            </label>
-            <input
-              className="shadow appearance-none border rounded border-accent w-full py-2 px-3 text-dark leading-tight focus:outline-secondary focus:shadow-outline placeholder-accent"
-              placeholder="End Date and Time"
-              type="text"
-              id="endDateTime"
-              name="endDateTime"
-              required
-            />
-          </div>
+            Date and Time 
+          </label>
+        </div>
+      <div className="display: inline-block">
+          <div className="display: inline-block px-2" >
+          <label
+            className="display: block text-dark text-sm font-bold mb-2  text-center"
+            htmlFor="startDateTime"
+          >
+            Start
+          </label>
+
+            <DateTimePicker />
+ 
+        </div>
+     
+          <div className="display: inline-block">
+          <label
+            className=" display: block text-dark text-sm font-bold mb-2  text-center"
+            htmlFor="endDateTime"
+          >
+            End
+          </label>
+            <DateTimePicker />
+ 
+      
+      </div>
+      </div>
+
+         
 
           <div className="mb-4">
             <label
@@ -171,10 +233,11 @@ function CreateEventForm() {
             </label>
             <input
               className="shadow appearance-none border rounded border-accent w-full py-2 px-3 text-dark leading-tight focus:outline-secondary focus:shadow-outline placeholder-accent"
-              placeholder="Event Price"
-              type="text"
-              id="price"
-              name="price"
+             
+             type="text" name="price" id="price" pattern="^\$\d{1,3}(,\d{3})*(\.\d+)?$" value="" data-type="currency" placeholder="$1,000,000.00"
+            
+         
+
               required
             />
           </div>
@@ -210,22 +273,59 @@ function CreateEventForm() {
               required
             />
           </div>
+
           <div className="mb-4">
             <label
               className="block text-dark text-sm font-bold mb-2"
-              htmlFor="description"
+              htmlFor="virtual"
             >
-              Event Description
+              Will this be a Virtual Event?
+            </label>
+            <div >
+
+            <div
+    onClick={() => displayText("yes")}
+              className={virtual ? ("display: inline-block shadow appearance-none border rounded border-accent  py-2 px-3 w-20 mx-2 text-center bg-primary") : ("display: inline-block shadow appearance-none border rounded border-accent  py-2 px-3 w-20 mx-2 text-center")}
+              placeholder="Virtual or In Person"
+              // type="text"
+              // name="virtual"
+              // required
+              // id="virtual"
+            >
+              Yes
+              </div>
+            <div
+             onClick={() => displayText("no")}
+              className={virtual ? ("display: inline-block shadow appearance-none border rounded border-accent  py-2 px-3 w-20 mx-2 text-center ") : ("display: inline-block shadow appearance-none border rounded border-accent  py-2 px-3 w-20 mx-2 text-center bg-primary")}
+              placeholder="Virtual or In Person"
+              // type="text"
+              // name="virtual"
+              // required
+              // id="virtual"
+            >
+No
+            </div>
+            </div>
+          </div>
+
+{!virtual ? ( <div className="mb-4">
+            <label
+              className="block text-dark text-sm font-bold mb-2"
+              htmlFor="location"
+            >
+              Location
             </label>
             <input
               className="shadow appearance-none border rounded border-accent w-full py-2 px-3 text-dark leading-tight focus:outline-secondary focus:shadow-outline placeholder-accent"
-              placeholder="Event Description"
-              type="text"
-              id="description"
-              name="description"
+              placeholder="Location"
+              name="location"
               required
+              type="text"
+              id="location"
             />
-          </div>
+          </div>) : (<></>)}
+         
+
           <div className="flex items-center justify-between">
             <button
               className="bg-dark hover:bg-secondary text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline m-auto"
