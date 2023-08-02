c748026941133349d6dc2fc086df5c2cc8f6f829
DanielleM <dlmedeiro@gmail.com>
2023-08-02 00:07:27 -0700

    style: add responsivness

--- src/app/events/details/page.tsx
+++ src/app/events/details/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 // Purpose: List out all event details
 
 // Functionality:
@@ -6,20 +6,15 @@
 // - Provide registration option -> onClick show registration confirmation (RegistrationComplete popup Module)
 // Updated registration based on a users registration status
 
-import React , {useEffect, useState} from 'react'
-import Image from 'next/image'
-import baseball from "../../../utils/images/baseball.webp"
-
-
+import React, { useEffect, useState } from "react";
+import Image from "next/image";
+import baseball from "../../../utils/images/baseball.webp";
 
 // import eventService from '@/app/features/EventService'
 
 // {"_id":"64c82df01662e48507781c97","name":"Event 2","is_virtual":false,"location":"New York","startDatetime":"2023-08-02T18:00:00.000Z","endDatetime":"2023-08-02T20:00:00.000Z","price":20,"tags":["entertainment"],"creatorId":"60d5ecb8b48738759f80f842","managedBy":["60d5ecb8b48738759f80f843"],"imgUrl":"http://example.com/event2.png","description":"This is a description for Event 2"}
 
-
-
 function EventDetailPage() {
-
   interface EventModel {
     name: string;
     is_virtual: boolean;
@@ -33,8 +28,8 @@ function EventDetailPage() {
     imgUrl: string;
     description: string;
   }
-  const  InitialState = {
-    name: '',
+  const InitialState = {
+    name: "",
     is_virtual: true,
     location: "",
     startDatetime: "",
@@ -45,122 +40,103 @@ function EventDetailPage() {
     managedBy: "",
     imgUrl: "",
     description: "",
-  }
+  };
 
   // temp
-    const EVENTS_URL = "https://api-vni1.onrender.com/events/64c82df01662e48507781c97 "
+  const EVENTS_URL =
+    "https://api-vni1.onrender.com/events/64c82df01662e48507781c97 ";
 
-  
-    const [event, setEvent] = useState<EventModel>(InitialState)
-    const [start, setStart] = useState<string>("")
-    const [end, setEnd] = useState<string>("")
-
-
-  
+  const [event, setEvent] = useState<EventModel>(InitialState);
+  const [start, setStart] = useState<string>("");
+  const [end, setEnd] = useState<string>("");
 
   useEffect(() => {
-    
-    const getEvent = async() => {
+    const getEvent = async () => {
+      const response = await fetch(EVENTS_URL);
+      const json = await response.json();
 
-        const response = await fetch(EVENTS_URL)
-        const json = await response.json();
-        
-        setEvent(json.event)
+      setEvent(json.event);
 
-        const startDate = new Date(json.event.startDatetime)
-        const endDate = new Date(json.event.endDatetime)
-        const formattedStartDateTime = new Intl.DateTimeFormat('en-US', {
-          year: 'numeric',
-          month: 'long',
-          day: 'numeric',
-          hour: 'numeric',
-          minute: 'numeric',
+      const startDate = new Date(json.event.startDatetime);
+      const endDate = new Date(json.event.endDatetime);
+      const formattedStartDateTime = new Intl.DateTimeFormat("en-US", {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+        hour: "numeric",
+        minute: "numeric",
+        hour12: true,
+      }).format(startDate);
+
+      setStart(formattedStartDateTime);
+
+      const startDatePortion = startDate.toISOString().slice(0, 10);
+      const endDatePortion = endDate.toISOString().slice(0, 10);
+
+      if (startDatePortion !== endDatePortion) {
+        let formattedEndDateTime = new Intl.DateTimeFormat("en-US", {
+          year: "numeric",
+          month: "long",
+          day: "numeric",
+          hour: "numeric",
+          minute: "numeric",
           hour12: true,
-        }).format(startDate);
-        
-        setStart(formattedStartDateTime)
+        }).format(endDate);
+        setEnd(formattedEndDateTime);
+      } else {
+        let formattedEndDateTime = new Intl.DateTimeFormat("en-US", {
+          hour: "numeric",
+          minute: "numeric",
+          hour12: true,
+        }).format(endDate);
 
-        const startDatePortion = startDate.toISOString().slice(0, 10);
-        const endDatePortion = endDate.toISOString().slice(0, 10);
-        
-        if(startDatePortion !== endDatePortion){
-          let formattedEndDateTime = new Intl.DateTimeFormat('en-US', {
-            year: 'numeric',
-            month: 'long',
-            day: 'numeric',
-            hour: 'numeric',
-            minute: 'numeric',
-            hour12: true,
-          }).format(endDate);
-          setEnd(formattedEndDateTime)
-        } else {
-          let formattedEndDateTime = new Intl.DateTimeFormat('en-US', {
-            hour: 'numeric',
-            minute: 'numeric',
-            hour12: true,
-          }).format(endDate);
-
-          setEnd(formattedEndDateTime)
-        }
-
-       
-
-
-        
-
-
-        console.log(endDate)
-  
-        return response
+        setEnd(formattedEndDateTime);
       }
 
-    getEvent()
+      return response;
+    };
+
+    getEvent();
 
     // .catch(console.log(error))
-
-  }, [])
-
-
+  }, []);
 
   return (
+    <div className="overflow-hidden mt-24 h-full min-h-screen">
+      <div className=" flex flex-row my-8 mx-4 xl:mx-32 flex-wrap">
+        <div className="m-auto mx-4 basis-full">
+          <h1 className="my-4  text-3xl xs:text-5xl sm:text-6xl md:text-8xl text-center">
+            {event.name}
+          </h1>
 
-<div className="mt-24 h-full min-h-screen">
-
-    <div className="m-auto mx-4">
-     <h1 className = "my-4 text-2xl sm:text-6xl lg:text-8xl  text-center">{event.name}</h1>
-
-      <h1 className = " w-full text-lg sm:text-lg lg:text-4xl text-center " >{start} to {end}</h1>
-      
-      </div>
-   
-    
-    <div className=" flex flex-row my-8 mx-4" >
-      <Image  className=" basis-1/2 h-auto w-2/5 m-auto " alt="event image" src={baseball} ></Image>
-      <div className="px-2 py-2 flex flex-col basis-1/2 ">
-      <div className = "h-1/2 text-2xl text-center flex flex-col items-center">
-      <h1  className = "h-1/3" >Location: {event.location}</h1>
-     <h1  className = "h-1/3" >Cost: ${event.price}</h1>
-      <h1  className = "h-1/3"  >Hosted By: {event.creatorId}</h1>
-
+          <h1 className=" my-8 w-full text-lg xs:text-1xl sm:text-2xl md:text-3xl lg:text-4xl text-center ">
+            {start} to {end}
+          </h1>
         </div>
-        <div  className="py-4 px-4 text-center text-2xl h-1/3">
-      <h1 >{event.description}</h1>
-
+        <Image
+          className=" basis-full lg:basis-1/2 h-2/5 w-auto lg:h-auto lg:w-1/4 m-auto "
+          alt="event image"
+          src={baseball}
+        ></Image>
+        <div className="px-2 py-2 flex flex-col basis-full lg:basis-1/2 ">
+          <div className="sm:px-8 lg:h-1/2 text-lg sm:text-2xl text-left flex flex-col py-4">
+            <h1 className="py-2">Location: {event.location}</h1>
+            <h1 className=" py-2">Cost: ${event.price}</h1>
+            <h1 className=" py-2">Hosted By: {event.creatorId}</h1>
+            <h1 className=" py-2">{event.description}</h1>
+            <button
+              className="bg-dark hover:bg-secondary text-white font-bold py-4 px-4 my-4 rounded focus:outline-none focus:shadow-outline m-auto text-1xl sm:text-4xl"
+              type="submit"
+            >
+              Register Today!
+            </button>
+          </div>
         </div>
-
-      <button  className="bg-dark hover:bg-secondary text-white font-bold py-4 px-4 rounded focus:outline-none focus:shadow-outline m-auto text-4xl"
-              type="submit">Registration</button>
       </div>
-
     </div>
-</div>
-
 
     /* {"_id":"64c82df01662e48507781c97","name":"Event 2","is_virtual":false,"location":"New York","startDatetime":"2023-08-02T18:00:00.000Z","endDatetime":"2023-08-02T20:00:00.000Z","price":20,"tags":["entertainment"],"creatorId":"60d5ecb8b48738759f80f842","managedBy":["60d5ecb8b48738759f80f843"],"imgUrl":"http://example.com/event2.png","description":"This is a description for Event 2"} */
-
-
- 
-  )
+  );
 }
 
-export default EventDetailPage
+export default EventDetailPage;
